b4e9ac86ab2fadb27a36458d618c99d814c98a85
annisafaras <89978961+annisafaras@users.noreply.github.com>
2022-12-28 19:47:06 +0700

    bagian annisa faras

---
+++ app/src/main/java/id/ac/unand/lunch/DonePengajuanKP.kt
@@ -0,0 +1,11 @@
+package id.ac.unand.lunch
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DonePengajuanKP : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_done_pengajuan_kp)
+    }
+}
