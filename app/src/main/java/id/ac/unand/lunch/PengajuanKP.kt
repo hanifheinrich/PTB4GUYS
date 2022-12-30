e7bf0aee921b295567bd76f9b852913ad604126f
annisafaras <89978961+annisafaras@users.noreply.github.com>
2022-12-30 12:02:19 +0700

    bagian annisafaras

--- app/src/main/java/id/ac/unand/lunch/PengajuanKP.kt
+++ app/src/main/java/id/ac/unand/lunch/PengajuanKP.kt
@@ -7,5 +7,6 @@ class PengajuanKP : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pengajuan_kp)
+        getSupportActionBar()?.hide()
     }
 }
